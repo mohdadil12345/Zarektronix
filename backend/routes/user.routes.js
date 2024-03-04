4f12c3fed165150e033da07a37e7d1c4c0b9f0b2
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-05 02:28:55 +0530

    update signup backend

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -10,25 +10,55 @@ const bcrypt = require("bcrypt")
 
 
 
-userRouter.post("/register", async (req, res) => {
-
-    const { password, email, name, mode } = req.body
-
-    try {
-
-        bcrypt.hash(password, 5, async (err, hash) => {
-
-            const user = new UserModel({ email, password: hash, name, mode })
-            await user.save()
-            res.status(200).json({ msg: "Registration succesffull", user })
-
-        })
 
 
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
+
+
+
+    userRouter.post("/register", async (req, res) => {
+
+        const { password, email, name, mode } = req.body
+
+        try {
+            const user = await UserModel.findOne({ email })
+    
+            if (user) {
+    
+                res.status(400).json({ msg: "User already exist, please login" })
+    
+            } else {
+                bcrypt.hash(password, 5, async (err, hash) => {
+                    if (hash) {
+                        const user = new UserModel({ email, password: hash, name, mode })
+                        await user.save()
+                        res.status(200).json({ msg: "Registration succesffull", user })
+                    }
+                })
+            }
+    
+        } catch (error) {
+            res.status(400).json({ error: error.message })
+        }
+    })
+    
+
+
+    // const { password, email, name, mode } = req.body
+
+    // try {
+
+    //     bcrypt.hash(password, 5, async (err, hash) => {
+
+    //         const user = new UserModel({ email, password: hash, name, mode })
+    //         await user.save()
+    //         res.status(200).json({ msg: "Registration succesffull", user })
+
+    //     })
+
+
+    // } catch (error) {
+    //     res.status(400).json({ error: error.message })
+    // }
 
 
 
